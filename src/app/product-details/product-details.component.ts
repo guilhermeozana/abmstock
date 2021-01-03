847d0945bae04d05c63462f0c642e20c9caf724a
Guilherme Ozana <guilhermeozana@hotmail.com>
2021-01-03 14:04:18 -0300

    criado o component product-details

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from 'src/product';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  id: number;
+  product: Product;
+
+  constructor(private route: ActivatedRoute, private router: Router,
+    private productService: ProductService) { }
+
+  ngOnInit() {
+    this.product = new Product();
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.productService.getProduct(this.id)
+    .subscribe(data => {
+      console.log(data);
+      this.product = data;
+    }, error => console.log(error));
+  }
+
+  list(){
+    this.router.navigate(['products']);
+  }
+
+}
